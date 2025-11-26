7617ce34db7f43fe67791b277eb3f8da5c82aac2
Abdelrahman-Elnagar <126884720+Abdelrahman-Elnagar@users.noreply.github.com>
2025-11-26 07:54:27 +0100

    chnaged time limits

--- RunOnCluster/04_run_dl.sh
+++ RunOnCluster/04_run_dl.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=300G
-#SBATCH --time=72:00:00
+#SBATCH --time=2:00:00
 
 source /opt/bwhpc/common/devel/miniforge/24.11.0-py3.12/etc/profile.d/conda.sh
 
